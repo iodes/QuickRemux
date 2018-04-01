52cadbecda17674c0240301869f725c73587fb4c
SO HYUN SEOP <iodesme@gmail.com>
2018-04-02 06:46:26 +0900

    드래그 앤 드롭 표시 구현

---
+++ QuickRemux/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace QuickRemux.Converters
+{
+    public class CountToVisibilityConverter : BaseValueConverter<int, Visibility>
+    {
+        public override Visibility Convert(int value, object parameter)
+        {
+            return value > 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public override int ConvertBack(Visibility value, object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
